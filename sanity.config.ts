7f2bde566903525409dbc76b66152f9842fc64d5
antonio-lopez <36496887+antonio-lopez@users.noreply.github.com>
2024-11-11 08:08:35 -0800

    build: add sanity and cloudinary dependencies and configs

---
+++ sanity.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { schema } from "./src/sanity/schemaTypes";
+import { cloudinarySchemaPlugin } from "sanity-plugin-cloudinary";
+
+export default defineConfig({
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  plugins: [structureTool(), cloudinarySchemaPlugin()],
+  schema,
+});
